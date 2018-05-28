c24c41307a668fe6cce8a46b711371712ce55f71
DESKTOP-JAQF34Q\Tyler <Cuervotyler1@gmail.com>
2018-05-28 17:12:02 -0400

    Added search API

--- AddContact.php
+++ AddContact.php
@@ -15,7 +15,7 @@
 	$conn = new mysqli("localhost", "root_suvrat", "cop4331!", "Contacts_Suvrat");
 	if ($conn->connect_error)
 	{
-		returnWithError( $conn->connect_error );
+		sendResultInfoAsJson("Connection Error");
 	}
 	else
 	{                              /* Data fields */
@@ -23,28 +23,15 @@
      . $contactState . ",'" . $contactCity .  ",'" . $contactAddress . ",'" $contactZipcode ",'" . $contactNickName ."')";
 		if( $result = $conn->query($sql) != TRUE )
 		{
-			returnWithError( $conn->error );
+			sendResultInfoAsJson($conn->error);
 		}
 		$conn->close();
 	}
   // Everything here should transfer without error
-	returnWithError("");
+	sendResultInfoAsJson("Connection Success");
 
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-
 ?>
