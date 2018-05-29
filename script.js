748dba31c20e7f9e2a23d23633ff84227d2e0cca
Ikenna Acholonu <ikacholonu@gmail.com>
2018-05-29 15:04:31 -0400

    Added more comments on the sha1(msg) to fully understand this more.

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ function doLogin()
 	var password = document.getElementById("loginPassword").value;
     
     //add the sha1 to take the password and send it off to the server
+    sha1(password);
 	
 	document.getElementById("loginResult").innerHTML = "";
 	
@@ -162,11 +163,13 @@ function ShowPass() {
 function sha1(msg) //found this online... I and just reading throught this to see if this could work.
 //it have a 20 character salt added to the hash before sending this to the server
 {
+    //function used for later use
     function rotl(n,s)
     {
         return n<<s|n>>>32-s;
     };
     
+    //function used for later use
     function tohex(i)
     {
         for(var h="", s=28;;s-=4)
@@ -176,7 +179,7 @@ function sha1(msg) //found this online... I and just reading throught this to se
         }
     };
     
-    var H0=0x67452301, H1=0xEFCDAB89, H2=0x98BADCFE, H3=0x10325476, H4=0xC3D2E1F0, M=0x0ffffffff;
+    var H0=0x67452301, H1=0xEFCDAB89, H2=0x98BADCFE, H3=0x10325476, H4=0xC3D2E1F0, M=0x0ffffffff; //salt
     
     var i, t, W=new Array(80), ml=msg.length, wa=new Array();
     msg += String.fromCharCode(0x80);
@@ -187,15 +190,18 @@ function sha1(msg) //found this online... I and just reading throught this to se
     for(i=0;i<msg.length;i+=4)
         wa.push(msg.charCodeAt(i)<<24|msg.charCodeAt(i+1)<<16|msg.charCodeAt(i+2)<<8|msg.charCodeAt(i+3));
     
-    while(wa.length%16!=14) wa.push(0);
-        wa.push(ml>>>29),wa.push((ml<<3)&M);
+    while(wa.length%16!=14)
+        wa.push(0);
+        wa.push(ml>>>29), wa.push((ml<<3)&M);
     
     for( var bo=0;bo<wa.length;bo+=16 )
     {
         for(i=0;i<16;i++)
             W[i]=wa[bo+i];
+        
         for(i=16;i<=79;i++)
             W[i]=rotl(W[i-3]^W[i-8]^W[i-14]^W[i-16],1);
+        
         var A=H0, B=H1, C=H2, D=H3, E=H4;
         
         for(i=0 ;i<=19;i++)
@@ -209,6 +215,7 @@ function sha1(msg) //found this online... I and just reading throught this to se
         
         for(i=60;i<=79;i++)
             t=(rotl(A,5)+(B^C^D)+E+W[i]+0xCA62C1D6)&M, E=D, D=C, C=rotl(B,30), B=A, A=t;
+        
         H0=H0+A&M; H1=H1+B&M; H2=H2+C&M; H3=H3+D&M; H4=H4+E&M;
     }
     return tohex(H0)+tohex(H1)+tohex(H2)+tohex(H3)+tohex(H4);
