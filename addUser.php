17c4bdab2157dc9f7cb9355b5fdba7e676c632c8
Greg Eugene <g.eugene25@gmail.com>
2018-06-06 20:12:34 -0400

    no message

--- addUser.php
+++ addUser.php
@@ -1,32 +1,42 @@
 <?php
+	$inData = getRequestInfo();
 
-$conn = new mysqli('localhost','root_suvrat','cop4331!','Contacts_Suvrat');
+	$userName = $inData["Username"];
+	$passWord = $inData["Password"];
 
-//test connection
-if($conn->connect_error) {
-  die ("Connection Error: " . $conn->connect_error);
-}
 
-if(isset($_POST['user']) && isset($_POST['password'])) {
+	$conn = new mysqli("localhost", "root_suvrat", "cop4331!", "Contacts_Suvrat");
+	if ($conn->connect_error || $userName == null)
+	{
+		returnWithError($conn->connect_error);
+	}
+	else
+	{
+		$sql = "insert INTO Users (Username, Password) VALUES (' $userName ', ' $passWord ')";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError($conn->error);
+		}
+		$conn->close();
+	}
 
-  $pWord = $_POST['password'];
-  $newUser = $_POST['username'];
-}
+	//returnWithError("");
 
-  $checkUsername = mysqli_query("SELECT * FROM Users WHERE Username = '$newUser'");
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
 
-  if(mysqli_num_rows($checkUsername) >= 1)
-  {
-    sendResultInfoAsJson("Error! Username already exists.");
-  }
-  else
-      {
-        $registerUser = mysqli_query("INSERT INTO Users (Username, Password) VALUES ('$newUser','$pWord')");
-      }
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
 
-  if($registerUser)
-      {
-        sendResultInfoAsJson("Your account was successfully created!");
-      }
-  $conn->close();
+	function returnWithError($err)
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+
 ?>
